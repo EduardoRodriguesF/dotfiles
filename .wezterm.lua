de2e445aebada61a78799248d87d7b362c2fbf49
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-04-15 21:03:42 -0300

    [wezterm] add wezterm config files
    This sorta deprecates Alacritty configs, but they will be kept for a
    while.
    
    Contains my recently submitted [Omni for
    WezTerm](https://github.com/EduardoRodriguesF/wezterm) color scheme.

---
+++ .wezterm.lua
@@ -0,0 +1,37 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Configs
+
+-- Colors
+config.color_scheme = 'Omni'
+
+-- Fonts
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 12
+
+-- No need for prompts while using tmux
+config.window_close_confirmation = 'NeverPrompt'
+
+-- Hide window stuff
+config.window_decorations = "NONE"
+config.use_fancy_tab_bar = false
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.show_tab_index_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Window size
+config.initial_cols = 160
+config.initial_rows = 40
+
+return config
